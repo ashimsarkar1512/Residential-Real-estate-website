4add7de6249101fbc31498aa2600ee51f644feab
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-04-15 02:43:22 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,14 +6,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3VuHeCZLWySvJ_rymXVYZVdfkr5IRWYM",
-  authDomain: "residential-real-estate-auth.firebaseapp.com",
-  projectId: "residential-real-estate-auth",
-  storageBucket: "residential-real-estate-auth.appspot.com",
-  messagingSenderId: "984515236737",
-  appId: "1:984515236737:web:84b49d2ddc4ce2c25b4e80"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
